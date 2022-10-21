dcf85fc98591eaff36ce0da50e9af2d5b6b102be
Jyotiramparalkar <paralkarjyotiram@gmail.com>
2022-10-21 16:28:37 +0530

    added Cab invoice Generator UC4 calculate invoice service

--- src/main/java/com/bridgelabz/Ride.java
+++ src/main/java/com/bridgelabz/Ride.java
@@ -1,10 +1,8 @@
 package com.bridgelabz;
 
-public class Ride
-{
+public class Ride {
     double distance;
     int time;
-
     public Ride(double distance, int time) {
         this.distance = distance;
         this.time = time;
